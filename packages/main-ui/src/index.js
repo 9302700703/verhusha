e537d60e6c074d5986282508f2877c4105ba2166
9302700703 <Vladislav.smirnov@phystech.edu>
2020-02-11 18:41:20 +0300

    Добавил redux

--- packages/main-ui/src/index.js
+++ packages/main-ui/src/index.js
@@ -1,31 +1,29 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Ttt from 'containers'
-// import { Router, Route, Switch } from 'react-router'
-import { HashRouter, Link } from 'react-router-dom'
-import Route from 'react-router-dom/es/Route'
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux'
+import { routerReducer, routerMiddleware } from 'react-router-redux'
+import { Provider } from 'react-redux'
+
+import Root from './Root'
 
 import '../../_styles/_main.global.less'
 
+const store = createStore(
+  combineReducers({
+    router: routerReducer
+  }),
+  {},
+  compose(
+    applyMiddleware(
+      routerMiddleware({}),
+
+    )
+  )
+)
+
 render(
-  <div className='ttt'>
-    <HashRouter>
-      <Route exact path='/' component={(props) => <div>
-        {Object.keys(props).map((key, index) => {
-          return <div key={index}>
-            {key} {props[key]
-              ? typeof props[key] === 'string'
-                ? props[key]
-                : Object.keys(props[key]).map((key2, index2) => {
-                  return <div style={{ paddingLeft: 20 }}>{key2}: {typeof props[key][key2]}</div>
-              })
-              : 'undefined'
-            }
-          </div>
-        })}
-      </div>}/>
-      <Route exact path='/ttt' component={() => <Link to='/'>---</Link>}/>
-    </HashRouter>
-  </div>,
+  <Provider store={{}}>
+    <Root/>
+  </Provider>,
   document.getElementById('root')
 )
