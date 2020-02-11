00534689ac7bfb649295b17563e43f10b1d5e74c
9302700703 <Vladislav.smirnov@phystech.edu>
2020-02-11 17:51:04 +0300

    Добавил react-router

--- packages/main-ui/src/index.js
+++ packages/main-ui/src/index.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import { render } from 'react-dom'
 import Ttt from 'containers'
+// import { Router, Route, Switch } from 'react-router'
+import { HashRouter, Link } from 'react-router-dom'
+import Route from 'react-router-dom/es/Route'
 
 import '../../_styles/_main.global.less'
 
 render(
   <div className='ttt'>
-    <Ttt/>
+    <HashRouter>
+      <Route exact path='/' component={(props) => <div>
+        {Object.keys(props).map((key, index) => {
+          return <div key={index}>
+            {key} {props[key]
+              ? typeof props[key] === 'string'
+                ? props[key]
+                : Object.keys(props[key]).map((key2, index2) => {
+                  return <div style={{ paddingLeft: 20 }}>{key2}: {typeof props[key][key2]}</div>
+              })
+              : 'undefined'
+            }
+          </div>
+        })}
+      </div>}/>
+      <Route exact path='/ttt' component={() => <Link to='/'>---</Link>}/>
+    </HashRouter>
   </div>,
   document.getElementById('root')
 )
